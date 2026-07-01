20e5a27bc53627a53a3d6972be0ba3d4daad1a5d
Talha Ansar <talhabajwa.x1@gmail.com>
2026-07-01 15:54:38 +0500

    cleaning

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -1,12 +1,9 @@
 from fastapi import APIRouter
 from schemas.user_schema import CreateUser,LoginRequest
-from passlib.context import CryptContext
-from database import save_user,login_user
-from authentication.auth import create_access_token
 from services.user_service import save_user as su,login as l
 from fastapi import HTTPException
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 
 
 router = APIRouter()
